f700bc850f487063d87116e4ecfa48923e2bf93e
dino <dino.alie@Gmail.com>
2019-08-11 00:20:25 +0700

    Initial Commit

---
+++ MonitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MonitTest
+//
+//  Created by Dino Ali on 11/08/19.
+//  Copyright © 2019 Dino Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
